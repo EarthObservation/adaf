32aaa073cde39b5170a6cff5ea4798e738b4d358
Nejc Čož <coz.nejc@gmail.com>
2023-11-16 11:36:27 +0100

    Update requirements
    Removed RVT scripts and added rvt_py to requirements.txt, added GDAL wheels file, updated setup.py script

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup, find_packages
 
 
 def parse_requirements(file):
-    return sorted(set(
-        line.partition('#')[0].strip()
-        for line in open(os.path.join(os.path.dirname(__file__), file))
-    ) - set(''))
+    return sorted(
+        set(
+            line.partition('#')[0].strip()
+            for line
+            in open(os.path.join(os.path.dirname(__file__), file))
+            if not line.startswith(".")
+        ) - set('')
+    )
 
 
 setup(
